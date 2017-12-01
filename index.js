e6bb24d3c6446ec4a9d6b5d1886c30d2e937d2b9
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-01 12:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function produceDrivingRange(blockRange) {
   return function(startPoint, endPoint) {
     let blockStart = Number(startPoint.slice(0, 2));
     let blockEnd = Number(endPoint.slice(0, 2));
+    console.log (blockEnd);
     let travelDistance = blockEnd - blockStart;
     if (travelDistance <= blockRange) {
-      return 'within range by ' + (travelDistance - blockRange);
+      return 'within range by ' + (blockRange - travelDistance);
     }
     else {
       return travelDistance + ' blocks out of range';
     }
+
   }
 }
