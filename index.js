3ed7726984dc1c8084ba0b5f4bf387a161109d24
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-01 12:38:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function(startPoint, endPoint) {
+    let blockStart = Number(startPoint.slice(0, 2));
+    let blockEnd = Number(endPoint.slice(0, 2));
     if ((startPoint - endPoint) <= blockRange) {
       return true
     }
